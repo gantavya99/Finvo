cdc2e6d3797746cf33c6f653bbca3b176195fba9
Gantavya Saraswat <gantavya99@gmail.com>
2024-04-17 01:49:09 +0530

    First Commit

---
+++ TrackFire/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Landing from "./pages/Landing Page/Landing";
+
+export default function App() {
+  return (
+        <>
+    <Landing />
+      </>
+  )
+  
+}
